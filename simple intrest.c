0ea4496c4ad975e34bbc1f1a7209ed5f533ab2fb
athiyafirdose123 <mrdevil00779@gmail.com>
2025-05-12 16:02:11 +0530

    Create simple intrest.c

---
+++ simple intrest.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+float p,t,r,si;
+int main(){
+    printf("enter the values of p ,t and r:");
+    scanf("%f%f%f",&p,&r,&t);
+    si=(p*t*r)/100;
+    printf("the simple interst is=%f",si);
+    
+}
