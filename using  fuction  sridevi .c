e8de26fd63f55f25c0674d3eb53607796e495bcb
athiya firdose <mrdevil00779@gmail.com>
2025-05-13 15:44:01 +0530

    Create using  fuction  sridevi .c

---
+++ using  fuction  sridevi .c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int sridevi(int a ,int b)
+{
+    printf(" athiya  ");
+    printf(" 7-11-2006 ");
+    printf(" 18 ");
+    printf( " 7795747559 ");
+    return a*b;
+ 
+}
+int main()
+{
+    sridevi( 5,10);
+    printf("%d");
+    return 0;
+}
+    
