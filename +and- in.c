1caffce596131e3b819994da388c3a7494279f8e
athiya firdose <mrdevil00779@gmail.com>
2025-05-13 16:11:57 +0530

    Create +and- in.c

---
+++ +and- in.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main(){
+    int number;
+    printf("enter a number:");
+    scanf("%d",&number);
+    if(number>0) {
+        printf("the number is positive.\n");
+    }
+    else if(number <0){
+        printf("the number is negative.\n");
+    } else {
+        printf("the number is zero,\n");
+    }
+}
