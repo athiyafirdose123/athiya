106e01001e327e57a8fc0b688f1dbf5a0bed6e00
athiya firdose <mrdevil00779@gmail.com>
2025-05-13 16:22:59 +0530

    Create sum,diffrence,product,\in .c

---
+++ sum,diffrence,product,\in .c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int a,b,sum,diffrence,product,qutiont;
+    printf("enter the value for a");
+    scanf("%d",&a);
+    printf("enter the value for b");
+    scanf("%d",&b);
+    sum=a+b;
+    diffrence=a-b;
+    product=a*b;
+    qutiont=a/b;
+    printf("%d\n%d\n%d\n",sum,diffrence,product,qutiont);
+    return 0;
+}
