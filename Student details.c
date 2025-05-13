c69f9a7f358405b42505fa58bd7a9b0ba83d009e
athiya firdose <mrdevil00779@gmail.com>
2025-05-13 14:17:27 +0530

    Create Student details.c

---
+++ Student details.c
@@ -0,0 +1,21 @@
+#include <studio,h>
+
+-struct studen {
+    char name[50];
+    int age;
+    char usn[20];
+    float height ;
+    float wheigh 
+    char collage [100
+};
+
+-int main() {
+    struct student s
+    
+   //input student details 
+    printf("enter student name:");
+    fgets(s.name,sizeof(s.name).stin);
+
+   printf("enter age:");
+   scanf("%d",&s.age);
+}
