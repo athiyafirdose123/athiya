4e474c0f87cbb9c6005796cc6a50f853cdcacb9e
athiya firdose <mrdevil00779@gmail.com>
2025-05-13 15:45:38 +0530

    Create without any int()in shridevi.c

---
+++ without any int()in shridevi.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int sridevi()
+{
+    printf(" athiya  ");
+    printf(" 7-11-2006 ");
+    printf(" 18 ");
+    printf( " 7795747559 ");
+    
+ 
+}
+int main()
+{
+    sridevi();
+    
+    return 0;
+}
