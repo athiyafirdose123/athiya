7253299c323b9e29787c70463433ae64d4172a17
athiya firdose <mrdevil00779@gmail.com>
2025-05-13 13:58:27 +0530

    Create Interger.c

---
+++ Interger.c
@@ -0,0 +1,12 @@
+#include <studio
+int   main()
+{
+    int num1,num2,num; 
+    printf("enter first number")
+    scanf("%d",&num1 );
+    printf("enter second number ")
+    scanf("%d",  &num2);
+    sum  = num1 + num2;
+    printf("sum of  %d and %d is % /",num1,num2,num);
+    return 0;
+}
