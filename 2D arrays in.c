496fc82b38b341703f242d259f1c1791d4f0c4e6
athiya firdose <mrdevil00779@gmail.com>
2025-05-14 11:56:02 +0530

    Create 2D arrays in.c

---
+++ 2D arrays in.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int a,b;
+int athu[2][3]={
+    {4,5,3},
+    {9,5,4}
+};
+int main(){
+    
+    for(a=0;a<2;a++){
+        for(b=0;b<3;b++){
+            printf("%d\n",athu[a][b]);
+        }
+        return 0;
+        }
+    }
