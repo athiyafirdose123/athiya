21252fd5ad87ad86b51a71688323246839a0e37a
athiya firdose <mrdevil00779@gmail.com>
2025-05-14 21:11:30 +0530

    Create All Even numbers.c

---
+++ All Even numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+void print_even(int arr[], int size) {
+    for(int i = 0; i < size; i++) {
+        if(arr[i] % 2 == 0)
+            printf("%d ", arr[i]);
+    }
+}
+
+int main() {
+    int nums[] = {1, 4, 7, 8, 10};
+    print_even(nums, 5); // Output: 4 8 10
+    return 0;
+}
