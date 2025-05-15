83ac56b79ecf19d5dafffe32e067a05a209885af
athiya firdose <mrdevil00779@gmail.com>
2025-05-15 09:49:29 +0530

    Update Use logically oparator .c

--- Use logically oparator .c
+++ Use logically oparator .c
@@ -1,8 +1,8 @@
-if (a > 0 && b > 0) // true if both a and b are positive
+if (a > 0 && b > 0) 
 
 
-if (a > 0 || b > 0) // true if either a or b is positive
+if (a > 0 || b > 0)
 
 
 
-if (!flag) // true if flag is false (0)
+if (!flag)
