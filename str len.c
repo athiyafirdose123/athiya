1cf6975c86fc2b15c8d1fed7e52b9e419a4be88d
athiya firdose <mrdevil00779@gmail.com>
2025-05-15 11:39:24 +0530

    Create str len.c

---
+++ str len.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char name[15];
+    printf("enter the name as :");
+    scanf("%s\n",&name);
+    printf("enter the name sadeeq:");
+    scanf("%s\n",&name);
+    printf("length:%lu\n",strlen(name));
+    return 0;
+}
+    
+
+
