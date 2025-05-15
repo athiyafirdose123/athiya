1b5525258ccc024eca7aac06ebab58fa2d0b9c6e
athiya firdose <mrdevil00779@gmail.com>
2025-05-15 23:18:29 +0530

    Create using strlen ().c

---
+++ using strlen ().c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+
+    printf("Enter a string: ");
+    fgets(str, sizeof(str), stdin);  // safer than gets()
+
+    // Remove newline character if present
+    str[strcspn(str, "\n")] = '\0';
+
+    int length = strlen(str);
+    printf("Length of the string: %d\n", length);
+
+    return 0;
+}
