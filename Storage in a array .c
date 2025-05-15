3f112a1b7fec48702fbe4feaac286594ed63842d
athiya firdose <mrdevil00779@gmail.com>
2025-05-15 09:19:31 +0530

    Update Storage in a array .c

--- Storage in a array .c
+++ Storage in a array .c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
-// Function to calculate the average of n numbers
+
 float calculate_average(int arr[], int n) {
     int sum = 0;
     
@@ -14,19 +14,19 @@ float calculate_average(int arr[], int n) {
 int main() {
     int n;
 
-    // Ask the user for the number of elements
+
     printf("Enter the number of elements: ");
     scanf("%d", &n);
 
     int arr[n];
 
-    // Input array elements
+    
     printf("Enter %d numbers:\n", n);
     for (int i = 0; i < n; i++) {
         scanf("%d", &arr[i]);
     }
 
-    // Calculate and print the average
+
     float avg = calculate_average(arr, n);
     printf("Average = %.2f\n", avg);
 
