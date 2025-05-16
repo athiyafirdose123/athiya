b5dbfb2ec3792333724374fba7f6136a449e5ebb
athiya firdose <mrdevil00779@gmail.com>
2025-05-16 09:11:22 +0530

    Update Hellow.c

--- Hellow.c
+++ Hellow.c
@@ -1,16 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    // Defining a null-terminated string
     char str[] = "Hello";
-
-    // Printing the string
     printf("The string is: %s\n", str);
-
-    // Printing the individual characters including the null terminator (in memory)
     for (int i = 0; i < 6; i++) {
         printf("Character %d: '%c' (ASCII: %d)\n", i, str[i], str[i]);
     }
-
     return 0;
 }
