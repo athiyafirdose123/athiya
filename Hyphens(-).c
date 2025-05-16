b1f260d253f9d2547bc48340f77ce398df7f5134
athiya firdose <mrdevil00779@gmail.com>
2025-05-16 09:12:51 +0530

    Update Hyphens(-).c

--- Hyphens(-).c
+++ Hyphens(-).c
@@ -1,24 +1,17 @@
 #include <stdio.h>
 
 void replaceSpacesWithHyphens(char str[]) {
-    // Traverse the string
     for (int i = 0; str[i] != '\0'; i++) {
-        if (str[i] == ' ') {  // Check if the character is a space
-            str[i] = '-';      // Replace space with hyphen
+        if (str[i] == ' ') {  
+            str[i] = '-';     
         }
     }
 }
-
 int main() {
     char str[100];
-
     printf("Enter a string: ");
-    fgets(str, sizeof(str), stdin);
-
-    // Remove the newline character if it's included by fgets()
+    fgets(str, sizeof(str), string;
     str[strcspn(str, "\n")] = '\0';
-
-    // Replace spaces with hyphens
     replaceSpacesWithHyphens(str);
 
     printf("Modified string: %s\n", str);
