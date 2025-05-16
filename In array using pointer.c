c2682b1dd34df82906049c24fff80c4870795612
athiya firdose <mrdevil00779@gmail.com>
2025-05-16 10:22:53 +0530

    Update In array using pointer.c

--- In array using pointer.c
+++ In array using pointer.c
@@ -4,12 +4,9 @@ int main() {
     int arr[] = {10, 20, 30, 40, 50};
     int *ptr;
     int size = sizeof(arr) / sizeof(arr[0]);
-
-    ptr = arr; // Point to the first element of the array
-
     printf("Array elements using pointer:\n");
     for (int i = 0; i < size; i++) {
-        printf("%d ", *(ptr + i)); // Access elements using pointer arithmetic
+        printf("%d ", *(ptr + i)); 
     }
 
     return 0;
