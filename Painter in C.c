477636e141be8ea6fd2f71900a92ddc4cdcbf999
athiya firdose <mrdevil00779@gmail.com>
2025-05-16 09:16:19 +0530

    Update Painter in C.c

--- Painter in C.c
+++ Painter in C.c
@@ -2,12 +2,10 @@
 
 int main() {
     int x = 10;
-    int *p = &x;  // p stores the address of x
-
-    printf("Value of x: %d\n", x);       // 10
-    printf("Address of x: %p\n", &x);    // Address of x
-    printf("Value stored in p: %p\n", p); // Same address as &x
-    printf("Value pointed by p: %d\n", *p); // 10
-
+    int *p = &x;  
+    printf("Value of x: %d\n", x);       
+    printf("Address of x: %p\n", &x);    
+    printf("Value stored in p: %p\n", p); 
+    printf("Value pointed by p: %d\n", *p); 
     return 0;
 }
