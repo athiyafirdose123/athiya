c02cb2a30ef7a2fcda15af0e87563803c4a4cbe4
athiya firdose <mrdevil00779@gmail.com>
2025-05-16 09:10:19 +0530

    Update Fgets.c

--- Fgets.c
+++ Fgets.c
@@ -5,11 +5,7 @@ int main() {
 
     printf("Enter your full name: ");
     fgets(fullName, sizeof(fullName), stdin);
-
-    // Remove the newline character if present
     fullName[strcspn(fullName, "\n")] = '\0';
-
     printf("Your full name is: %s\n", fullName);
-
     return 0;
 }
