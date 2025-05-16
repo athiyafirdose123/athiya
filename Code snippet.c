5265d2cb95e3b21c46fb8b8aef36313ce37d0b3e
athiya firdose <mrdevil00779@gmail.com>
2025-05-16 07:25:11 +0530

    Update Code snippet.c

--- Code snippet.c
+++ Code snippet.c
@@ -2,12 +2,8 @@
 
 int main() {
     int num = 42;
-    int *ptr = &num; // ptr holds the address of num
-
-    // Dereferencing the pointer to access the value of num
+    int *ptr = &num; 
     printf("Value of num using pointer: %d\n", *ptr);
-
-    // Changing the value of num using the pointer
     *ptr = 100;
     printf("New value of num: %d\n", num);
 
