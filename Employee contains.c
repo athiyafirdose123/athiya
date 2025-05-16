e8ceec80ce45866dd1da090d1a5453b59df48ffc
athiya firdose <mrdevil00779@gmail.com>
2025-05-16 22:01:14 +0530

    Create Employee contains.c

---
+++ Employee contains.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+// Define Address structure
+struct Address {
+    char city[50];
+    int pin;
+};
+
+// Define Employee structure containing Address
+struct Employee {
+    char name[50];
+    int id;
+    struct Address address; // Nested structure
+};
+
+int main() {
+    struct Employee emp;
+
+    // Input employee details
+    printf("Enter employee name: ");
+    scanf(" %[^\n]", emp.name);
+
+    printf("Enter employee ID: ");
+    scanf("%d", &emp.id);
+
+    printf("Enter city: ");
+    scanf(" %[^\n]", emp.address.city);
+
+    printf("Enter pin code: ");
+    scanf("%d", &emp.address.pin);
+
+    // Display employee details
+    printf("\nEmployee Details:\n");
+    printf("Name: %s\n", emp.name);
+    printf("ID: %d\n", emp.id);
+    printf("City: %s\n", emp.address.city);
+    printf("Pin Code: %d\n", emp.address.pin);
+
+    return 0;
+}
