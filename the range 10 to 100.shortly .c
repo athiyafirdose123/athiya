ade7b8563a9d2e0b3a01311a67b7217fe122c1c4
athiya firdose <mrdevil00779@gmail.com>
2025-05-16 09:45:33 +0530

    Update the range 10 to 100.shortly .c

--- the range 10 to 100.shortly .c
+++ the range 10 to 100.shortly .c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 int main() {
     int num;
     printf("Enter a number: ");
     scanf("%d", &num);
-
     if (num >= 10 && num <= 100)
         printf("Within range\n");
     else
         printf("Out of range\n");
-
     return 0;
 }
