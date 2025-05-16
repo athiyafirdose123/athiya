815979e76a2d596d6d969e83aa78ca9bae5f50a6
athiya firdose <mrdevil00779@gmail.com>
2025-05-16 09:09:38 +0530

    Update Consonants in a string.c

--- Consonants in a string.c
+++ Consonants in a string.c
@@ -1,17 +1,14 @@
 #include <stdio.h>
-#include <ctype.h>  // For tolower()
-
+#include <ctype.h>  
 int main() {
     char str[100];
     int vowels = 0, consonants = 0;
-
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
-
     for (int i = 0; str[i] != '\0'; i++) {
-        char ch = tolower(str[i]);  // Convert to lowercase
+        char ch = tolower(str[i]);  
 
-        if (ch >= 'a' && ch <= 'z') {  // Check if it's an alphabet
+        if (ch >= 'a' && ch <= 'z') {  
             if (ch == 'a' || ch == 'e' || ch == 'i' ||
                 ch == 'o' || ch == 'u') {
                 vowels++;
