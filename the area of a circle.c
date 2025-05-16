892f94274e8ca6a45b6c274cf057fda8a1b054af
athiya firdose <mrdevil00779@gmail.com>
2025-05-16 09:44:59 +0530

    Update the area of a circle.c

--- the area of a circle.c
+++ the area of a circle.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
-#define PI 3.14159
-
 float area_of_circle(float radius) {
     return PI * radius * radius;
 }
-
 int main() {
     float r = 5.0;
     float area = area_of_circle(r);
