98e7d56593ee46fa3938de9cb2d88e0b0b68f907
athiya firdose <mrdevil00779@gmail.com>
2025-05-16 09:15:26 +0530

    Update Library function.c

--- Library function.c
+++ Library function.c
@@ -2,18 +2,13 @@
 
 int main() {
     char str[100];
-
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
-
-    // Convert to uppercase manually
     for (int i = 0; str[i] != '\0'; i++) {
         if (str[i] >= 'a' && str[i] <= 'z') {
             str[i] = str[i] - ('a' - 'A');  // Or subtract 32
         }
     }
-
     printf("Uppercase string: %s", str);
-
     return 0;
 }
