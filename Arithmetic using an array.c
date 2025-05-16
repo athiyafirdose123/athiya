2a30b4cc0a9d8faadb105b3d47a615da089a1f34
athiya firdose <mrdevil00779@gmail.com>
2025-05-16 09:08:09 +0530

    Update Arithmetic using an array.c

--- Arithmetic using an array.c
+++ Arithmetic using an array.c
@@ -2,14 +2,10 @@
 
 int main() {
     int arr[] = {10, 20, 30, 40, 50};
-    int *ptr = arr;  // points to the first element
-
+    int *ptr = arr;  
     printf("Accessing elements using pointer arithmetic:\n");
-
     for (int i = 0; i < 5; i++) {
-        // *(ptr + i) is equivalent to arr[i]
         printf("Element %d: %d (Address: %p)\n", i, *(ptr + i), (ptr + i));
     }
-
-    return 0;
+ return 0;
 }
