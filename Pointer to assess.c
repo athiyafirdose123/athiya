2ce9c8948fb639b035c0aadb6f9fc1051707664d
athiya firdose <mrdevil00779@gmail.com>
2025-05-17 13:19:29 +0530

    Update Pointer to assess.c

--- Pointer to assess.c
+++ Pointer to assess.c
@@ -1,13 +1,11 @@
 struct Student {
     char name[50];
     int roll;
-    float marks;
-};
+    float marks;};
 struct Student s1 = {"Alice", 101, 89.5};
-struct Student *ptr;   // Declare pointer to Student
-ptr = &s1;             // Store address of s1 in ptr
-
+struct Student *ptr;   
+ptr = &s1;          
 printf("Name: %s\n", ptr->name);
 printf("Roll: %d\n", ptr->roll);
 printf("Marks: %.2f\n", ptr->marks);
-printf("Name: %s\n", (*ptr).name);  // Same as ptr->name
+printf("Name: %s\n", (*ptr).name);
