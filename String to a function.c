02876c231c4b1cbd411e33dd817b80a1411e7f8b
athiya firdose <mrdevil00779@gmail.com>
2025-05-17 13:26:26 +0530

    Update String to a function.c

--- String to a function.c
+++ String to a function.c
@@ -1,19 +1,12 @@
 #include <stdio.h>
-
-// Function to print each character of the string
 void printCharacters(char str[]) {
-    // Loop through each character in the string
     for (int i = 0; str[i] != '\0'; i++) {
         printf("%c ", str[i]);
     }
     printf("\n");
 }
-
 int main() {
-    char str[] = "Hello, World!"; // Define a string
-
-    // Pass the string to the function
+    char str[] = "Hello, World!";
     printCharacters(str);
-
     return 0;
 }
