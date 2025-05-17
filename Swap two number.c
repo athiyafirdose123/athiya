ee88e86e63b5822e8e1a1a3be9dd2f0da61e050d
athiya firdose <mrdevil00779@gmail.com>
2025-05-17 13:27:42 +0530

    Update Swap two number.c

--- Swap two number.c
+++ Swap two number.c
@@ -1,20 +1,18 @@
 #include <stdio.h>
 
-// Function to swap values using pointers
+
 void swap(int *a, int *b) {
     int temp;
-    temp = *a;   // Store the value of *a
-    *a = *b;     // Assign value of *b to *a
-    *b = temp;   // Assign stored value to *b
-}
+    temp = *a;  
+    *a = *b;     
+    *b = temp;   }
+
 
 int main() {
     int num1 = 5, num2 = 10;
 
     printf("Before swapping:\n");
     printf("num1 = %d, num2 = %d\n", num1, num2);
-
-    // Call swap function with addresses of num1 and num2
     swap(&num1, &num2);
 
     printf("After swapping:\n");
