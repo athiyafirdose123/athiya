ff738f28f38ce6f5995b3c96db61f06de8069b57
athiya firdose <mrdevil00779@gmail.com>
2025-05-17 13:23:51 +0530

    Update Library function.c

--- Library function.c
+++ Library function.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 int main() {
     char str[100];
     printf("Enter a string: ");
     fgets(str, sizeof(str), stdin);
     for (int i = 0; str[i] != '\0'; i++) {
         if (str[i] >= 'a' && str[i] <= 'z') {
-            str[i] = str[i] - ('a' - 'A');  // Or subtract 32
+            str[i] = str[i] - ('a' - 'A'); 
         }
     }
     printf("Uppercase string: %s", str);
