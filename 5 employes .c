466ee162d84caf345648b67cfb4315c8bbe9f276
athiya firdose <mrdevil00779@gmail.com>
2025-05-17 09:06:58 +0530

    Update 5 employes .c

--- 5 employes .c
+++ 5 employes .c
@@ -1,30 +1,20 @@
 #include <stdio.h>
-
-// Define the structure
 struct Employee {
     char name[50];
     int id;
     float salary;
 };
-
 int main() {
-    struct Employee emp[5]; // Array of 5 structures
-
-    // Input details for each employee
+    struct Employee emp[5]; 
     for (int i = 0; i < 5; i++) {
         printf("\nEnter details for Employee %d:\n", i + 1);
-
         printf("Name: ");
-        scanf(" %[^\n]", emp[i].name); // To read string with spaces
-
+        scanf(" %[^\n]", emp[i].name); 
         printf("ID: ");
         scanf("%d", &emp[i].id);
-
         printf("Salary: ");
         scanf("%f", &emp[i].salary);
     }
-
-    // Display employee details
     printf("\nEmployee Details:\n");
     for (int i = 0; i < 5; i++) {
         printf("\nEmployee %d:\n", i + 1);
@@ -32,6 +22,5 @@ int main() {
         printf("ID: %d\n", emp[i].id);
         printf("Salary: %.2f\n", emp[i].salary);
     }
-
     return 0;
 }
