a8f015a806d16b955bc6fc198fafdea1a03adf16
athiya firdose <mrdevil00779@gmail.com>
2025-05-17 13:24:11 +0530

    Update Marks 75 passed if 40--75 and failed .c

--- Marks 75 passed if 40--75 and failed .c
+++ Marks 75 passed if 40--75 and failed .c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-
 int main() {
     int marks;
     printf("Enter marks: ");
     scanf("%d", &marks);
-
     if (marks > 75)
         printf("Distinction\n");
     else if (marks >= 40)
         printf("Pass\n");
     else
         printf("Fail\n");
-
     return 0;
 }
