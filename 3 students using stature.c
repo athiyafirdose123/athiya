fc6aa6edadcd72c39704994eabfd87d6c7cb48ea
athiya firdose <mrdevil00779@gmail.com>
2025-05-17 07:33:05 +0530

    Update 3 students using stature.c

--- 3 students using stature.c
+++ 3 students using stature.c
@@ -1,17 +1,12 @@
 #include <stdio.h>
-
-// Define the structure
 struct Student {
     char name[50];
     int roll;
     float marks;
 };
-
 int main() {
-    struct Student students[3]; // Array to store 3 students
+    struct Student students[3]; 
     float totalMarks = 0.0, average;
-
-    // Input student details
     for (int i = 0; i < 3; i++) {
         printf("\nEnter details for Student %d:\n", i + 1);
 
@@ -26,12 +21,7 @@ int main() {
 
         totalMarks += students[i].marks;
     }
-
-    // Calculate average
     average = totalMarks / 3.0;
-
-    // Display result
     printf("\nAverage Marks of 3 Students: %.2f\n", average);
-
     return 0;
 }
